16b236712431426cc12e6c3fc4174c2b976bdbcd
Osama M.Younus <osamayounus541@gmail.com>
2022-12-10 11:58:15 +0500

    box challenge completed

--- src/App.jsx
+++ src/App.jsx
@@ -270,10 +270,20 @@ import Box from './components/Box'
 
     const [squares,setSquares] = useState(boxes);
 
-    
+    function toggle(id){
+      setSquares(prevSquares => {
+        return prevSquares.map((square) => {
+          return square.id === id ? {...square, on: !square.on} : square
+        })
+      })
+    }
     
     const squareElements = squares.map(square=>(
-      <Box key = {square.id} on={square.on} />
+      <Box 
+      key={square.id}
+      on={square.on}
+      toggle={()=>toggle(square.id)}
+      />
         
       
     ))
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,19 +1,18 @@
-import { useState } from "react";
-
 function Box(props){
 
-    const [on, setOn] = useState(props.on);
+    
 
 
     const styles = {
-        backgroundColor: on ? "#222222" : "transparent"
+        backgroundColor: props.on ? "#222222" : "transparent"
     }
 
-    function toggle(){
-        setOn(prevOn => !prevOn)
-    }
+   
     return(
-        <div style={styles} className='box' onClick={toggle}>
+        <div 
+         style={styles}
+         className='box'
+        onClick={props.toggle}>
         
       </div>
     )
