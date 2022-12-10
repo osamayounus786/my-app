f9f6190df3913e613738dd8d1c7de461b849d663
Osama M.Younus <osamayounus541@gmail.com>
2022-12-10 15:20:07 +0500

    jokes initial

---
+++ src/components/datas.jsx
@@ -0,0 +1,13 @@
+// function Data(props){
+//     console.log(props)
+//   return(
+//     <>
+//     <img className="card-img" src={props.img} alt="wings" />
+//     <h2>{props.type}</h2>
+//     <p>{props.price}</p>
+
+//     </>
+//   )
+// }
+
+// export default Data;
