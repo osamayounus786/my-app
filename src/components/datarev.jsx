5030eb3ea2ae0666d2a33bb90dd00e8e968cbc80
Osama M.Younus <osamayounus541@gmail.com>
2022-12-10 14:35:22 +0500

    props concept and rev

---
+++ src/components/datarev.jsx
@@ -0,0 +1,21 @@
+export const data =[{
+    id:1,
+    name: "Osama",
+    fName: "M.Younus",
+    age: 25,
+    developer: true
+},
+{
+    id:2,
+    name: "Sameer",
+    fName: "Bux",
+    age: 24,
+    developer: true
+},
+{
+    id:3,
+    name: "Hassam",
+    fName: "Aslam",
+    age: 23,
+    developer: false
+},]
---
+++ src/components/propsrev.jsx
@@ -0,0 +1,16 @@
+
+function Props(props){
+   
+    return(
+        <>
+        {/* <h2>this is my brand {props.brand.model} and this is {props.brand.name}</h2> */}
+        <div>
+            <p>
+                my name is {props.name} my father name is {props.fName} my age is {props.age}
+            </p>
+            <hr />
+        </div>
+        </>
+    )
+}
+export default Props;
