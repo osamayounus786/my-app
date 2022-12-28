570efd7edaa389b43b5d6eb82b6939f38648be80
Osama M.Younus <osamayounus541@gmail.com>
2022-12-28 20:36:17 +0500

    usecontext concepts clear

--- src/components/ComA.jsx
+++ src/components/ComA.jsx
@@ -1,11 +1,17 @@
 import ComB from './ComB'
+import { createContext } from 'react';
+const BioData = createContext();
 function ComA(){
     return(
       <>
+      <BioData.Provider value={"Osama Younus"}>
       <ComB />
+
+      </BioData.Provider>
       
       </>
     )
    }
   
-   export default ComA;
+   export default ComA; 
+   export {BioData};
--- src/components/ComC.jsx
+++ src/components/ComC.jsx
@@ -1,7 +1,10 @@
+import { BioData } from "./ComA";
+import { useContext } from "react";
 function ComC(){
+    const myName = useContext(BioData);
     return(
       <>
-      <h1>hello this is ComC</h1>
+      <h1>hello this is  {myName}</h1>
       
       </>
     )
